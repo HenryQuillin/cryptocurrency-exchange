580543da06c638e26b80969806ce3ff000734540
Henry Quillin <henryquillin@gmail.com>
2021-10-16 15:16:55 -0500

    first commit

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+import "./Token.sol";
+
+contract EthSwap {
+  string public name = "EthSwap Instant Exchange";
+  Token public token;
+
+  constructor(Token _token) public {
+    token = _token;
+  }
+}
---
+++ test/EthSwap.test.js
@@ -0,0 +1,41 @@
+const Token = artifacts.require('Token')
+const EthSwap = artifacts.require('EthSwap')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether');
+}
+
+contract('EthSwap', (accounts) => {
+  let token, ethSwap
+
+  before(async () => {
+    token = await Token.new()
+    ethSwap = await EthSwap.new()
+    // Transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, tokens('1000000'))
+  })
+
+  describe('Token deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await token.name()
+      assert.equal(name, 'Chrono Coin')
+    })
+  })
+
+  describe('EthSwap deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await ethSwap.name()
+      assert.equal(name, 'EthSwap Instant Exchange')
+    })
+
+    it('contract has tokens', async () => {
+      let balance = await token.balanceOf(ethSwap.address)
+      assert.equal(balance.toString(), tokens('1000000'))
+    })
+  })
+
+})
